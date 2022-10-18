ca661e80faa41d768ef50fedfab5a06db83c03bf
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-10-18 10:00:52 -0700

    Printf2

--- printf/main.h
+++ printf/main.h
@@ -1,7 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h> 
+
+/**
+ * pr_fmt - struct name
+ * @fmt: index of function
+ * @func: pointer to function
+ */
+
+typedef struct pr_fmt
+{
+	char *fmt;
+	int (*func)(va_list list);
+}pr_fmt;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int func_percent(va_list list);
+int char_func(va_list list);
+int str_func(va_list list);
+int get_func(const char *format, va_list list, pr_fmt pr_format[]);
 
 #endif /*MAIN_H*/
