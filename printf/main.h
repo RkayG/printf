a49ca10f09316985aaabb9dd52c298a3b7ef0f04
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-10-19 00:45:55 -0700

    Printf2

--- printf/main.h
+++ printf/main.h
@@ -18,12 +18,14 @@ typedef struct pr_fmt
 	int (*func)(va_list list);
 }pr_fmt;
 
-int _putchar(char c);
+
 int _printf(const char *format, ...);
 int func_percent(va_list list);
 int char_func(va_list list);
 int str_func(va_list list);
 int get_func(const char *format, va_list list, pr_fmt pr_format[]);
 int dig_func(va_list list);
+int func_revstr(va_list list);
+int func_rot13(va_list list);
 
 #endif /*MAIN_H*/
